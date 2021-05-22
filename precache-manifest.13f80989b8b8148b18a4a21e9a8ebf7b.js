e53f6cc773b15d69ed19257bd91d7a5f06c09673
hari <hari720k@gmail.com>
2021-05-22 17:26:22 +0530

    test

---
+++ precache-manifest.13f80989b8b8148b18a4a21e9a8ebf7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2473a44f0a5e005a51840b2157e0f844",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98f0e117c6fd5b318ca0",
+    "url": "/static/css/main.bac4b7f1.chunk.css"
+  },
+  {
+    "revision": "4f02c898d90c3262a987",
+    "url": "/static/js/2.f5eef1c6.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.f5eef1c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98f0e117c6fd5b318ca0",
+    "url": "/static/js/main.cb76ec48.chunk.js"
+  },
+  {
+    "revision": "5823efb02d026ec3609d",
+    "url": "/static/js/runtime-main.1014f6ff.js"
+  }
+]);
